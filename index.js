da0c2a494c27243b78431abc72dfd841d1d12e81
hanifabeg <hanifa.beg@gmail.com>
2019-09-19 14:53:13 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+// CONFIGURE ENVIRONMENT VARIABLES
+require('dotenv').config()
+
+// REQUIRE VENDOR LIBS
+const express = require('express')
+const corsMiddleware = require('restify-cors-middleware')
+
+// INSTANTIATE APP
+const app = express()
+
+// CONFIGURE CORS
+const cors = corsMiddleware({
+    origins: ['*'],
+    allowHeaders: ['*']
+})
+
+// SETUP ROUTER
+const router = express.Router()
+router.use(cors.preflight)
+router.use(cors.actual)
+
+// REQUIRE SERVICES
+const twitter = require('./twitterAPI');
+
+router.get('/users/:id', (req, res) => {
+    twitter
+        .getTweetsForUser('', { count: 20 })
+        .then(tweets => res.send(tweets))
+        .catch(error => res.status(400).send(error))
+})
+
+// REGISTER ROUTES
+app.use('/api', router)
+
+// START SERVER
+app.listen(3010)
